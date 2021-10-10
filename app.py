9ffb578f22341a98bab52a452e9a2c9ec214cb79
ksathwik03 <iit2020241@iiita.ac.in>
2021-10-10 12:24:59 +0530

    revert

--- app.py
+++ app.py
@@ -241,7 +241,6 @@ def api_image_get(id):
                 if username not in views:
                     views.append(username)
                     images.update(tinydb.operations.set("views",views), doc_ids=[id])
-                images.update(tinydb.operations.increment("views"), doc_ids=[id])
 
     if not image:
         return "", 404
@@ -294,7 +293,7 @@ def api_image_info(id):
         "public": public,
         "likes": len(image.get("likes")),
         "liked": username and username in image.get("likes"),
-        "views": image.get("views")
+        "views": len(image.get("views"))
     }
 
     if public or owner == username:
@@ -355,7 +354,7 @@ def api_image_delete():
 
                     for image in imageList:
                         totalLikes += len(image.get("likes"))
-                        totalViews += image.get("views")
+                        totalViews += len(image.get("views"))
 
             return json.dumps({ "totalLikes": totalLikes, "totalViews": totalViews}), 200
         else:
@@ -760,7 +759,7 @@ def profile():
 
                 for image in imageList:
                     totalLikes += len(image.get("likes"))
-                    totalViews += image.get("views")
+                    totalViews += len(image.get("views"))
 
     return render_template(
         "profile.html",
@@ -794,7 +793,7 @@ def api_user_info(username):
 
                 for image in imageList:
                     totalLikes += len(image.get("likes"))
-                    totalViews += image.get("views")
+                    totalViews += len(image.get("views"))
 
     if not account:
         return json.dumps(None), 404
@@ -883,7 +882,7 @@ def upload():
                             "public": False,
                             "description": description,
                             "likes": [],
-                            "views": 0
+                            "views": [],
                         }
 
                         with dbLock:
